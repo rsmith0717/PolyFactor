1863577bc772268214c734c4ee0a0a8a5f131103
Roderick O Smith <smithroderick17@gmail.com>
2017-09-10 15:06:30 -0500

    added parsing regex

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,10 @@
 var button= document.getElementById('submit');
-var text = '';
+var polynomial = '';
+var arr = [];
 button.onclick= function(){
-    text = document.getElementById('inputbox').value;
-    console.log(text);
+    polynomial = document.getElementById('inputbox').value;
+    console.log(polynomial);
+    arr = polynomial.match(/(\+|\-)?[a-z0-9.^]+/gi);
+    console.log(arr.toString());
+    
 }
