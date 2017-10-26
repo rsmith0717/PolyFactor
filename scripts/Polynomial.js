6010adf19d518237b54eba0f90412564e9fc58c6
Roderick O Smith <smithroderick17@gmail.com>
2017-10-26 14:49:34 -0500

    Made most of the functions class based so they require no input.

--- scripts/Polynomial.js
+++ scripts/Polynomial.js
@@ -3,10 +3,10 @@ class Polynomial {
         this.polynomial = equation;
         this.tempoly = equation;
         this.arr = [];
-        this.polynomials = []
-        this.constants = []
-        this.sortpolysresults = []
-        this.zerotest = []
+        this.polynomials = [];
+        this.constants = [];
+        this.sortpolysresults = [];
+        this.zerotest = [];
         this.polyfacts = [];
         this.constfacts = [];
         this.pZero = 0; // potential zero
@@ -18,25 +18,26 @@ class Polynomial {
         console.log('The polynomial is: ' + this.polynomial);
         this.termsplit();
         console.log('The split terms are: ' + this.arr.toString());
+        console.log('The current equation is: ' + this.finalString);
 
-        this.polyorconst(this.arr, this.constants, this.polynomials);
+        this.polyorconst();
         console.log(this.polynomials.toString());
         console.log(this.constants.toString());
-        this.expsplit(this.polynomials, this.sortpolysresults);
-        if (this.sortpolysresults[0] > 2) {
+        this.sortpolysresults = this.expsplit();
+        if (this.sortpolysresults[0] > 2) { // checks if the first term's exponent is greater than ^2
             console.log(this.sortpolysresults.toString());
             this.bubbleSort(this.sortpolysresults, this.polynomials);
             console.log(this.polynomials.toString());
             descartes(this.tempoly);
-            this.zerotest = this.testTerms(this.polynomials, this.constants);
-            this.getFactors(this.zerotest, this.polyfacts, this.constfacts);
+            this.zerotest = this.testTerms();
+            this.getFactors();
             this.pZero = possibleZeros(this.polyfacts, this.constfacts);
             this.dividends = cosplit(this.polynomials, this.constfacts);
             var next = this.synthetic(this.pZero, this.dividends, this.finalString);
             var pass = rejoin(next);
             let poly2 = new Polynomial(pass, this.finalString);
             poly2.syntheticdivision();
-        } else {
+        } else { // quadratic needs to take place when the first term's exponent is ^2
 
         }
     }
@@ -67,21 +68,27 @@ class Polynomial {
     }
 
 
-    polyorconst(arr, constants, polynomials) {
-
-        arr.forEach(function (term) { //                      seperates the constant from the terms containing variables
-            var constant = /^\d+$/.test(term);
+    polyorconst() {
+        var constants = []
+        var polynomials = []
+        this.arr.forEach(function (term) { //                      seperates the constant from the terms containing variables
+            //var constant = /^\d+$/.test(term);
+            console.log(term);
             var constant = /^[-|+]?\d*\.?\d+$/.test(term);
+            console.log(constant);
             if (constant == true) {
                 constants.push(term);
             } else {
                 polynomials.push(term);
             }
         });
+        this.constants = constants;
+        this.polynomials = polynomials;
     }
 
-    expsplit(polynomials, results) { // need to add zeores between powers
-        polynomials.forEach(function (poly) { // splits the exponent from the terms containing variables
+    expsplit() { // need to add zeores between powers
+        var results = [];
+        this.polynomials.forEach(function (poly) { // splits the exponent from the terms containing variables
             var power = 0;
             if (poly.includes('^')) {
                 power = parseInt(poly.split('^')[1]);
@@ -90,27 +97,28 @@ class Polynomial {
             }
             results.push(power);
         })
+        return results;
     }
 
-    testTerms(polynomials, constants) { // takes the leading term of the polynomials and the constant term to pass to the getFactors method
+    testTerms() { // takes the leading term of the polynomials and the constant term to pass to the getFactors method
         var zerotest = [];
-        console.log(polynomials.toString());
+        console.log(this.polynomials.toString());
         //console.log(polynomials[0]);
-        var leadingterm = polynomials[0].split(/[a-zA-Z](.+)/)[0];
+        var leadingterm = this.polynomials[0].split(/[a-zA-Z](.+)/)[0];
         console.log('The leading polynomial term is: ' + leadingterm);
         zerotest.push(leadingterm);
-        if (constants[0] != null) {
-            zerotest.push(constants[0]);
+        if (this.constants[0] != null) {
+            zerotest.push(this.constants[0]);
         }
         console.log(zerotest.toString());
         return zerotest;
     }
 
-    getFactors(zerotest, polyfacts, constfacts) { // finds the common factors of the leading polynomial term and the constant term
+    getFactors() { // finds the common factors of the leading polynomial term and the constant term
         // console.log(zerotest[0]);
         //console.log(zerotest[1]);    
-        var factoree1 = parseInt(zerotest[0]); // the leading coefficient of greatest power polynomial
-        var factoree2 = parseInt(zerotest[1]); // the constant
+        var factoree1 = parseInt(this.zerotest[0]); // the leading coefficient of greatest power polynomial
+        var factoree2 = parseInt(this.zerotest[1]); // the constant
         console.log(factoree1);
         console.log(factoree2);
 
@@ -118,48 +126,48 @@ class Polynomial {
         if (Math.sign(factoree1) == 1) { // loop for positive polynomial terms
             for (var x = 0; x <= factoree1; x++) {
                 if (factoree1 % x == 0) {
-                    polyfacts.push(x)
+                    this.polyfacts.push(x)
                 }
             }
         } else if (Math.sign(factoree1) == -1) { // loop for factors of negative polynomial term
             for (var x = factoree1; x <= 0; x++) {
                 if (factoree1 % x == 0) {
-                    polyfacts.push(x)
+                    this.polyfacts.push(x)
                 }
             }
         }
         if (Math.sign(factoree2) == 1) { // loop for factors of positive constant term
             for (var x = 0; x <= factoree2; x++) {
                 if (factoree2 % x == 0) {
-                    constfacts.push(x)
+                    this.constfacts.push(x)
                 }
             }
         } else if (Math.sign(factoree2) == -1) { // loop for factors of negative constant term
             for (var x = factoree2; x <= 0; x++) {
                 if (factoree2 % x == 0) {
-                    constfacts.push(x)
+                    this.constfacts.push(x)
                 }
             }
         }
 
-        console.log(polyfacts.toString()); // factors of the polynomial coefficient
-        console.log(constfacts.toString()); // factors of the constant
+        console.log(this.polyfacts.toString()); // factors of the polynomial coefficient
+        console.log(this.constfacts.toString()); // factors of the constant
     }
 
-    cosplit(polynomials, constants) { // not working properly needs to push zeores
+    cosplit() { // not working properly needs to push zeores
         results = [];
-        console.log(polynomials.toString())
-        for (var x = polynomials.length - 1; x >= 0; x--) {
+        console.log(this.polynomials.toString())
+        for (var x = this.polynomials.length - 1; x >= 0; x--) {
             var pass = x + 1;
             console.log(pass);
-            if (polynomials[x].search('^' + (pass).toString())) {
-                results.push(polynomials[x].split('x')[0])
+            if (this.polynomials[x].search('^' + (pass).toString())) {
+                results.push(this.polynomials[x].split('x')[0])
             } else {
                 console.log('^' + (pass).toString())
                 results.push('0')
             }
         }
-        results.unshift(constants);
+        results.unshift(this.constfacts);
         console.log(results.toString())
         return results;
     }
@@ -167,48 +175,50 @@ class Polynomial {
     synthetic(pZero, dividends, finalString) { // works fine until division step
         var polynomials = '';
         var constants = '';
-        results = [];
-        console.log('The divisor is: ' + pZero.toString())
-        console.log('The dividends are: ' + dividends.toString())
-        dividends.forEach(function (element, i) {
+        var dividends = [];
+        var results = [];
+        console.log('The divisor is: ' + this.pZero.toString())
+        console.log('The dividends are: ' + this.dividends.toString())
+        this.dividends.forEach(function (element, i) {
             element = parseInt(element);
             //console.log(element);
             dividends[i] = element;
         });
-        console.log(dividends.toString())
-        var divisor = pZero;
+        this.dividends = dividends;
+        console.log(this.dividends.toString())
+        var divisor = this.pZero;
         var carry = 0;
 
-        results = division(pZero, dividends);
+        results = division(this.pZero, this.dividends);
         console.log(results.toString())
-        if (Math.sign(pZero) == 1) {
-            pZero = Math.abs(pZero);
-            finalString = finalString + '(x - ' + pZero.toString() + ')';
+        if (Math.sign(this.pZero) == 1) {
+            this.pZero = Math.abs(pZero);
+            this.finalString = this.finalString + '(x - ' + this.pZero.toString() + ')';
         }
         if (Math.sign(pZero) == -1) {
-            pZero = Math.abs(pZero);
-            finalString = finalString + '(x + ' + pZero.toString() + ')';
+           this. pZero = Math.abs(pZero);
+            this.finalString = this.finalString + '(x + ' + this.pZero.toString() + ')';
         }
-        console.log('The final equation is currently: ' + finalString + '(' + results + ')');
+        console.log('The final equation is currently: ' + this.finalString + '(' + this.results + ')');
         testTerms(results, constants);
         return results;
     }
 
-    division(pZero, dividends) {
-        length = dividends.length;
-        console.log(pZero);
+    division() {
+        length = this.dividends.length;
+        console.log(this.pZero);
         results = [];
         var carry = 0;
 
         for (var x = length - 1; x >= 0; x--) {
             //console.log('This is the ' + x + ' iteration of the loop');
             if (x == length - 1) {
-                carry = dividends[x];
+                carry = this.dividends[x];
                 results.push(carry)
                 //console.log('This is the ' + length + ' element')
             } else {
                 console.log(carry);
-                carry = (dividends[x] + (carry * pZero))
+                carry = (this.dividends[x] + (carry * this.pZero))
                 if (x != 0 && carry != 0) {
                     console.log(carry);
                     results.push(carry);
@@ -243,22 +253,22 @@ class Polynomial {
         results.reverse();
     }
 
-    possibleZeros(polyfacts, constfacts) {
+    possibleZeros() {
         var bigVar = 0;
         var smallVar = 0;
         var result = 0;
         var negRes = 0;
         var res = []
-        if (constfacts.length > polyfacts.length) { // find out which has more factors and use it as dividend
-            bigVar = constfacts.length
-            smallVar = polyfacts.length
+        if (this.constfacts.length > this.polyfacts.length) { // find out which has more factors and use it as dividend
+            bigVar = this.constfacts.length
+            smallVar = this.polyfacts.length
         } else {
-            bigVar = polyfacts.length
-            smallVar = constfacts.length
+            bigVar = this.polyfacts.length
+            smallVar = this.constfacts.length
         }
         for (var y = 0; y < smallVar; y++) { // loop runs until smallest array is done
             for (var x = 0; x < bigVar; x++) { // loop runs through biggest array on each increment of first loop
-                result = constfacts[x] / polyfacts[y]
+                result = this.constfacts[x] / this.polyfacts[y]
                 res.push(result)
                 negRes = (result - (result * 2))
                 res.push(negRes)
